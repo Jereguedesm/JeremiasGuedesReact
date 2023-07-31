33f944972361f1408273b93ea3e6b49eb0e654ef
Jeremias <jeremiasguedes04@gmail.com>
2023-07-30 23:21:48 -0300

    V-0.4.1

--- src/components/microComponents/Languages/Languages.jsx
+++ src/components/microComponents/Languages/Languages.jsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
- const Languages = () => {
+const Languages = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
